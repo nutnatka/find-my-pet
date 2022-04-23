6f3a1638bf510169b64ca07b65c1d025eb870c4a
Kate Haman <11kateh@gmail.com>
2022-04-23 22:04:12 +0200

    updated .gitignore :sparkles:

---
+++ db/schema.rb
@@ -0,0 +1,135 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_04_23_184032) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.bigint 'post_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_categories_on_post_id'
+  end
+
+  create_table 'comments', force: :cascade do |t|
+    t.string 'name'
+    t.text 'body'
+    t.bigint 'user_id', null: false
+    t.bigint 'post_id', null: false
+    t.string 'ancestry'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
+  end
+
+  create_table 'conversations', force: :cascade do |t|
+    t.integer 'recipient_id'
+    t.integer 'sender_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[recipient_id sender_id], name: 'index_conversations_on_recipient_id_and_sender_id', unique: true
+  end
+
+  create_table 'likes', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'post_id', null: false
+    t.bigint 'comment_id', null: false
+    t.bigint 'message_id', null: false
+    t.bigint 'place_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['comment_id'], name: 'index_likes_on_comment_id'
+    t.index ['message_id'], name: 'index_likes_on_message_id'
+    t.index ['place_id'], name: 'index_likes_on_place_id'
+    t.index ['post_id'], name: 'index_likes_on_post_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
+  end
+
+  create_table 'messages', force: :cascade do |t|
+    t.text 'body'
+    t.bigint 'user_id', null: false
+    t.bigint 'conversation_id', null: false
+    t.boolean 'read', default: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['conversation_id'], name: 'index_messages_on_conversation_id'
+    t.index ['user_id'], name: 'index_messages_on_user_id'
+  end
+
+  create_table 'pets', force: :cascade do |t|
+    t.string 'name'
+    t.string 'species'
+    t.string 'breed'
+    t.string 'sex'
+    t.boolean 'sterilized'
+    t.date 'date_of_birth'
+    t.string 'color'
+    t.text 'additional_info'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_pets_on_user_id'
+  end
+
+  create_table 'places', force: :cascade do |t|
+    t.string 'name'
+    t.string 'address'
+    t.float 'latitude'
+    t.float 'longitude'
+    t.bigint 'pet_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['pet_id'], name: 'index_places_on_pet_id'
+  end
+
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.string 'status'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'name'
+    t.boolean 'admin', default: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
+
+  add_foreign_key 'categories', 'posts'
+  add_foreign_key 'comments', 'posts'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'likes', 'comments'
+  add_foreign_key 'likes', 'messages'
+  add_foreign_key 'likes', 'places'
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users'
+  add_foreign_key 'messages', 'conversations'
+  add_foreign_key 'messages', 'users'
+  add_foreign_key 'pets', 'users'
+  add_foreign_key 'places', 'pets'
+  add_foreign_key 'posts', 'users'
+end
