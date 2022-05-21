efd7771350c228edda2d7e2729cf4e105fe5305f
Kate Haman <11kateh@gmail.com>
2022-05-21 20:04:28 +0200

    updated post create functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(category_id: params[:category_id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     root 'welcome#index'
     match '/users/:id', to: 'users#show', via: 'get'
     get 'users', to: 'welcome#index'
+    post 'posts#index', to: 'posts#new', as: 'new_posts_post'
   end
 end
