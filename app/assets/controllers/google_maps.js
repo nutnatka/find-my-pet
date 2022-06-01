fafec34650e2bf204047de526d2773642a49ce3a
nutnatka <chernienko.natalka@gmail.com>
2022-06-01 23:02:05 +0300

    google map

--- app/assets/controllers/google_maps.js
+++ app/assets/controllers/google_maps.js
@@ -1,71 +1,62 @@
+let map;
+
 function initMap() {
-    const center = new google.maps.LatLng(49.427673, 32.050320);
+    const center = {lat: 49.427673, lng: 32.050320};
 
     // Declare your map options
     const mapOptions = {
-        zoom: 12,
+        zoom: 18,
         center: center,
-        const: my_pet = {},
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         disableDoubleClickZoom: true,
         mapTypeControl: false,
         streetViewControl: false,
     };
 
-
     // Create a map in the #map HTML element, using the declared options
-    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    map.addListener("dblclick", (e) => {
-        placeMarkerAndPanTo(e.latLng, map);
-    });
+    // map.addListener("dblclick", (e) => {
+    //     placeMarkerAndPanTo(e.latLng);
+    // });
 
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 300,
-        maximumAge: 0
-    };
-    navigator.geolocation.getCurrentPosition(showPosition, error, options);
-
-    function showPosition(position) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        if (position.coords == undefined) {
+            return;
+        }
+        var latLng = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        };
         new google.maps.Marker({
-            // position: position,
-            position: position.coords,
+            position: latLng,
             map: map,
         });
-    }
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-
+        map.setCenter(latLng);
+    });
 
 }
 
+function placeMarkerAndPanTo(latLng) {
 
-function placeMarkerAndPanTo(latLng, map) {
-
-    const petInfo = `<h1>INfo</h1>`
-
-    const infoWindow = new google.maps.InfoWindow({
-        content: petInfo,
-    });
-
-    const marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-    });
-
-    marker.addListener("click", () => {
-        infoWindow.open({
-            anchor: marker,
-            map,
-            shouldFocus: false,
-        });
-    });
-
-    map.panTo(latLng);
+    // const petInfo = `<h1>INfo</h1>`
+    //
+    // const infoWindow = new google.maps.InfoWindow({
+    //     content: petInfo,
+    // });
+    //
+    // const marker = new google.maps.Marker({
+    //     position: latLng,
+    //     map: map,
+    // });
+    //
+    // marker.addListener("click", () => {
+    //     infoWindow.open({
+    //         anchor: marker,
+    //         map,
+    //         shouldFocus: false,
+    //     });
+    // });
+    //
+    // map.panTo(latLng);
 }
-
-
-window.initMap = initMap;
