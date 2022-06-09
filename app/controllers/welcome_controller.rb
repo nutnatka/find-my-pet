9f0144965be685a1e1bb65c6c1fb30d5df0b1985
Kate Haman <11kateh@gmail.com>
2022-06-09 08:24:57 +0200

    added slider

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,6 @@ class WelcomeController < ApplicationController
   def index
     @pets = Pet.all
     @pets_to_adopt = @pets.to_adopt.order(updated_at: :desc).take(6)
+    @success_posts = Post.where(category_id: 11)
   end
 end
