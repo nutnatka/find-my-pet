44215ff4935d0f9021ee171f3637325c8792f038
Kate Haman <11kateh@gmail.com>
2022-06-17 11:03:43 +0200

    added fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @q = @user.pets.ransack(params[:q])
+    @pets = @q.result.order(:created_at).page(params[:page]).per(5)
+
+    redirect_to @user if @q.result.page(params[:page]) == 0
   end
 end
