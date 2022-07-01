55586bbaf96ced3c116b32a8044b82261be877ab
Kate Haman <11kateh@gmail.com>
2022-07-01 12:05:17 +0200

    lost posts hp fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,6 @@ class WelcomeController < ApplicationController
     @pets = Pet.all
     @pets_to_adopt = @pets.to_adopt.order(created_at: :desc).take(6)
     @success_posts = Post.with_success_stories.where(status: 'published').order(created_at: :desc).take(10)
-    @lost_posts = Post.with_lost_pet.order(created_at: :desc).take(3)
+    @lost_posts = Post.with_lost_pet.where(status: 'published').order(created_at: :desc).take(3)
   end
 end
